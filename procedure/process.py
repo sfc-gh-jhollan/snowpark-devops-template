23ed1e3a9987794692fd7a24814bb1aac2221ef1
Jeff Hollan <jeff.hollan@snowflake.com>
2022-09-20 13:58:15 -0700

    convert to vectorized udf

--- procedure/process.py
+++ procedure/process.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 from snowflake.snowpark import Session, DataFrame
-from snowflake.snowpark.types import FloatType, IntegerType
+from snowflake.snowpark.types import PandasSeriesType, PandasDataFrameType, IntegerType, FloatType
 from snowflake.snowpark.functions import col, year
 from sklearn.linear_model import LinearRegression
 import pandas as pd
@@ -40,11 +40,11 @@ def train_linear_regression_model(input_pd: pd.DataFrame) -> LinearRegression:
     return model
 
 def register_udf(model, session):
-    def predict_pce(predictYear: int) -> float:
-        return model.predict([[predictYear]])[0].round(2).astype(float)
+    def predict_pce(ps: pd.Series) -> pd.Series[float]:
+        return ps.transform(lambda x: model.predict([[x]])[0].round(2).astype(float))
     session.udf.register(predict_pce,
-                        return_type=FloatType(),
-                        input_type=IntegerType(),
+                        return_type=PandasSeriesType(FloatType()),
+                        input_type=PandasSeriesType(IntegerType()),
                         packages= ["pandas","scikit-learn"],
                         is_permanent=True, 
                         name="predict_pce_udf", 
