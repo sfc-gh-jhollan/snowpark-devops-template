0f1b3881cc21a64b54fb2f9aff68bc79b60030ed
Jeff Hollan <jeff.hollan@snowflake.com>
2022-09-21 20:45:29 -0700

    modifying input types

--- procedure/process.py
+++ procedure/process.py
@@ -42,11 +42,11 @@ def train_linear_regression_model(input_pd: pd.DataFrame) -> LinearRegression:
     return model
 
 def register_udf(model, session):
-    def predict_pce(ps: pd.DataFrame) -> pd.Series:
-        return ps[0].transform(lambda x: model.predict([[x]])[0].round(2).astype(float))
+    def predict_pce(ps: pd.Series) -> pd.Series:
+        return ps.transform(lambda x: model.predict([[x]])[0].round(2).astype(float))
     session.udf.register(predict_pce,
                         return_type=PandasSeriesType(FloatType()),
-                        input_type=PandasDataFrameType([IntegerType()]),
+                        input_types=[PandasSeriesType(IntegerType())],
                         packages= ["pandas","scikit-learn"],
                         is_permanent=True, 
                         name="predict_pce_udf", 
